12ee373e1133406e8999604a85034d90ffea7774
urjuhh <github@klaabu.pri.ee>
2016-03-13 00:11:37 +0200

    asd

--- src/main/java/com/urjuhh/prefab/Prefab.java
+++ src/main/java/com/urjuhh/prefab/Prefab.java
@@ -8,6 +8,7 @@ import com.urjuhh.prefab.proxy.IProxy;
 import com.urjuhh.prefab.reference.Reference;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -34,7 +35,8 @@ public class Prefab {
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
-        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        //FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        MinecraftForge.EVENT_BUS.register(new ConfigurationHandler());
 
         ModBlocks.init();
         ModBlocks.register();
